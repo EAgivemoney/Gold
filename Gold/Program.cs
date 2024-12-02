1170133c3d2e380f4e457cdb4686b21b0b7c0df9
EAgivemoney <Tannneronyx@gmail.com>
2024-12-02 09:48:00 +0100

    forgot to commit Program

---
+++ Gold/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Gold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gold Adventure!");
+            Menu.DisplayMainMenu();
+        }
+    }
+}
